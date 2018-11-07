bcc0de6361ea60498b6e1acd92f4b057297d80f5
Rafael Mardones <rmardn@gmail.com>
2018-11-07 13:17:59 -0200

    Production build for GitHub pages

---
+++ build/precache-manifest.edb2868eeb84b17f043776a87813a892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1917fbe58d3d366e622",
+    "url": "/static/js/main.b1917fbe.chunk.js"
+  },
+  {
+    "revision": "839cc000004adf479ec3",
+    "url": "/static/js/1.839cc000.chunk.js"
+  },
+  {
+    "revision": "b1917fbe58d3d366e622",
+    "url": "/static/css/main.514b37d6.chunk.css"
+  },
+  {
+    "revision": "96e56078bc4da6e1ea8fe479664b4a9f",
+    "url": "/index.html"
+  }
+];
