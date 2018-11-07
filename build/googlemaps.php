ddb07f7dcf0bca7f28b0426de7d3f603caaf7237
Rafael Mardones <rmardn@gmail.com>
2018-11-07 13:17:59 -0200

    Revert "Production build for GitHub pages"
    This reverts commit bcc0de6361ea60498b6e1acd92f4b057297d80f5.

--- build/googlemaps.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://maps.googleapis.com/maps/api/place/queryautocomplete/json?key=AIzaSyCPOggMNd0hWwm34KGzHvqLI2HV_byYoH4&inputtype=textquery&input=" . $_GET['input']);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$output = curl_exec($ch);
-	curl_close($ch);
-	echo $output;
-?>
