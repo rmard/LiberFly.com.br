87d6a573429173eddd15d3b584d58a1663242f51
Rafael Mardones <rmardn@gmail.com>
2018-11-08 15:54:28 -0200

    SideNav implemented

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,47 @@
 import React from 'react'
+import './navbar.css';
 
 class Navbar extends React.Component {
 
-  render = () => (
-    <div className="navbar-fixed">
-      <nav className="white">
-        <div className="nav-wrapper">
-          <a href="/" className="brand-logo center hide-on-small-only">
-              <img src="logo-liberfly.png" className='nav-big-logo' alt="Logo"/>
-          </a>
-          <a href="/" className="brand-logo left hide-on-med-and-up">
-            <div className='nav-small-logo'></div>
-          </a>
-        </div>
-      </nav>    
+  render = () => (    
+    <div>
+      <div className="navbar-fixed">
+        <nav className="white">
+          <div className="nav-wrapper">
+            <a href="#!" data-target="slide-out" className="blue-text sidenav-trigger left"><i className="material-icons">menu</i></a>
+            <a href="/" className="brand-logo center">
+                <img src="logo-liberfly.png" className='nav-big-logo' alt="Logo"/>
+            </a>
+            <a href="/" className="brand-logo hide">
+              <div className='nav-small-logo'></div>
+            </a>
+          </div>
+        </nav>         
+      </div>
+      <ul id="slide-out" className="sidenav left-align">
+        <li><div className="user-view">
+          <div className="background">
+            &nbsp;
+          </div>
+          { /*
+          <a href="#user"><img className="circle" src="optimized-ross-parmly-25230-unsplash.jpg"/></a>
+          <a href="#name"><span className="white-text name">John Doe</span></a>
+          <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
+           */ }
+        </div></li>
+        {//<li><a href="#!">Início</a></li>
+        }
+        <li><a href="/about">Quem somos</a></li>
+        <li><a href="#!" className='disabled'>Conheça seus direitos</a></li>
+        <li><a href="#!" className='disabled'>Blog de viagem</a></li>
+        <li><a href="#!" className='disabled'>Notícias</a></li>
+        <li><a href="#!" className='disabled'>Ajuda</a></li>
+        <li><a href="/claim">Envie sua reclamação</a></li>
+        <li><div className="divider"></div></li>
+        {//<li><a className="subheader">Subheader</a></li>
+        }
+        <li><a className="btn waves-effect waves-light" href="/">Calcule sua indenização</a></li>
+      </ul> 
     </div>
   )
 }
