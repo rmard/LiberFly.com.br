8fc16a3f31d6940dd1d6e01169278fcacebfeffa
Rafael Mardones <rmardn@gmail.com>
2018-11-19 15:46:46 -0200

    Security risk correction
    react/jsx-no-target-blank as described in https://mathiasbynens.github.io/rel-noopener

--- src/ButtonChat.js
+++ src/ButtonChat.js
@@ -4,8 +4,9 @@ import {isMobile} from 'react-device-detect'
 export default () => (
 	<a href={`https://${isMobile?'api':'web'}.whatsapp.com/send?phone=+552799635-8409&text=Olá! Tenho a seguinte dúvida: `}
 		target="_blank" 
-      	style={{padding: '0px'}}
-      	className='btn btn-flat btn-large grey lighten-2 grey-text text-darken-2 waves-effect waves-light btn-chat'>
+    rel="noopener noreferrer"
+  	style={{padding: '0px'}}
+  	className='btn btn-flat btn-large grey lighten-2 grey-text text-darken-2 waves-effect waves-light btn-chat'>
         <span className='wpp-icon left'>&nbsp;</span>
       	{window.innerWidth > 390 ? 
       		'TIRE DÚVIDAS COM UM ATENDENTE':
