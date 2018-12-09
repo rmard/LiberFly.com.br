5b053f186947554d28b0241adec647dd8d494989
Rafael Mardones <rmardn@gmail.com>
2018-12-09 09:26:28 -0200

    Disabling location predictions
    As requested

--- src/LocationInput.js
+++ src/LocationInput.js
@@ -1,55 +1,19 @@
 import React from 'react'
 import {DebounceInput} from 'react-debounce-input';
 
-const endpoint = '//novosite.liberfly.com.br/googlemaps.php?input=';
-
 class LocationInput extends React.Component {
-	state = {
-		predictions: []
-	}
-	handleChange = (event) => {
-		this.props.onChange(event.target.value);
-		this.setState({value: event.target.value});
-		fetch(endpoint+event.target.value)
-			.then((res)=>(res.json()))
-			.then((data)=>{
-				//console.log(data);
-				this.setState({predictions: data.predictions});
-			});
-	}
-	selectPrediction = (event) => {
-		this.setState({predictions: []});
-		this.setState({
-			predictions: []
-		});
-		this.props.onChange(event.target.innerHTML);
-	}
 	render = () => {
 		return (
       <div className="input-field location-input">
         <i className="material-icons prefix indigo-text text-darken-2">place</i>
         <DebounceInput
-          minLength={3}
-          debounceTimeout={500} 
-        	onChange={this.handleChange}
+          	minLength={3}
+          	debounceTimeout={500} 
+        	onChange={(e)=>this.props.onChange(e.target.value)}
         	value={this.props.value} 
         	type="text" 
         	placeholder={this.props.label} 
-        	className="autocomplete"/>
-        	{this.state.predictions.length>0 && (
-						<ul 
-							className="autocomplete-content dropdown-content" 
-							tabIndex="0" 
-							style={{display: 'block', 
-								opacity: 1,
-								top: '50px'}}>
-							{this.state.predictions.map((prediction)=>(
-								<li key={prediction.description} onClick={this.selectPrediction}>
-									<span>{prediction.description}</span>
-								</li>
-							))}
-						</ul> 
-        	)}       	
+		/>     	
       </div>
 		);
 	}
